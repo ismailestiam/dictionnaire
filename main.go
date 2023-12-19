e93e33b3e7f4b6764898c2c07b2081fcc9aa6ce2
ismailestiam <148719043+ismailestiam@users.noreply.github.com>
2023-12-19 16:19:12 +0100

    main.go

--- main.go
+++ main.go
@@ -1,47 +1,21 @@
 package main
 
 import (
+	"example/dictionnaire/dictionary"
 	"fmt"
 )
 
-type dictionnaire struct {
-	data map[string]string
-}
-
-func NewDictionary() *dictionnaire {
-	return &dictionnaire{data: make(map[string]string)}
-}
-
-func (d *dictionnaire) Add(key, value string) {
-	d.data[key] = value
-}
-
-func (d *dictionnaire) Get(key string) string {
-	return d.data[key]
-}
-
-func (d *dictionnaire) Remove(key string) {
-	delete(d.data, key)
-}
-
-func (d *dictionnaire) List() {
-	fmt.Println("Listing key-value pairs:")
-	for key, value := range d.data {
-		fmt.Printf("Key: %s, Value: %s\n", key, value)
-	}
-}
-
 func main() {
+	// Utilisation de la classe Dictionnaire
+	dict := dictionary.NewDictionnaire()
 
-	dict := NewDictionary()
+	dict.Ajouter("estiam", "ecole")
+	dict.Ajouter("ismail", "etudiant")
 
-	dict.Add("estiam", "ecole")
-	dict.Add("ismail", "etudiant")
+	fmt.Println("Obtenir 'estiam':", dict.Obtenir("estiam"))
 
-	fmt.Println("Get 'estiam':", dict.Get("estiam"))
+	dict.Lister()
 
-	dict.List()
-
-	dict.Remove("ismail")
-	dict.List()
+	dict.Supprimer("ismail")
+	dict.Lister()
 }
