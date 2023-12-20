2a16d01f446c0ae4ef5197efd38515ed7b8d04c8
ismailestiam <148719043+ismailestiam@users.noreply.github.com>
2023-12-20 10:37:37 +0100

    Dictionary.go

--- Dictionary.go
+++ Dictionary.go
@@ -1,30 +1,128 @@
 package dictionary
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+type Entry struct {
+	Mot        string `json:"mot"`
+	Definition string `json:"definition"`
+}
 
 type Dictionnaire struct {
-	data map[string]string
+	filePath string
+	entries  []Entry
 }
 
-func NewDictionnaire() *Dictionnaire {
-	return &Dictionnaire{data: make(map[string]string)}
-}
-
-func (d *Dictionnaire) Ajouter(mot, definition string) {
-	d.data[mot] = definition
-}
-
-func (d *Dictionnaire) Get(mot string) string {
-	return d.data[mot]
-}
-
-func (d *Dictionnaire) Supprimer(mot string) {
-	delete(d.data, mot)
-}
-
-func (d *Dictionnaire) Lister() {
-	fmt.Println("Listing key-value pairs:")
-	for key, value := range d.data {
-		fmt.Printf("Key: %s, Value: %s\n", key, value)
+func NewDictionnaire(filePath string) *Dictionnaire {
+	return &Dictionnaire{
+		filePath: filePath,
+		entries:  nil,
 	}
+}
+
+func (d *Dictionnaire) loadFromFile() error {
+	fileData, err := ioutil.ReadFile(d.filePath)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(fileData, &d.entries)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (d *Dictionnaire) saveToFile() error {
+	fileData, err := json.MarshalIndent(d.entries, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(d.filePath, fileData, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (d *Dictionnaire) Add(mot, definition string) error {
+	err := d.loadFromFile()
+	if err != nil {
+		return err
+	}
+
+	newEntry := Entry{Mot: mot, Definition: definition}
+	d.entries = append(d.entries, newEntry)
+
+	err = d.saveToFile()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (d *Dictionnaire) Get(mot string) (string, error) {
+	err := d.loadFromFile()
+	if err != nil {
+		return "", err
+	}
+
+	for _, entry := range d.entries {
+		if entry.Mot == mot {
+			return entry.Definition, nil
+		}
+	}
+
+	return "", fmt.Errorf("Mot non trouvé : %s", mot)
+}
+
+func (d *Dictionnaire) Remove(mot string) error {
+	err := d.loadFromFile()
+	if err != nil {
+		return err
+	}
+
+	var updatedEntries []Entry
+	found := false
+	for _, entry := range d.entries {
+		if entry.Mot == mot {
+			found = true
+		} else {
+			updatedEntries = append(updatedEntries, entry)
+		}
+	}
+
+	if !found {
+		return fmt.Errorf("Mot non trouvé : %s", mot)
+	}
+
+	d.entries = updatedEntries
+
+	err = d.saveToFile()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (d *Dictionnaire) List() error {
+	err := d.loadFromFile()
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Listing key-value pairs:")
+	for _, entry := range d.entries {
+		fmt.Printf("Mot: %s, Definition: %s\n", entry.Mot, entry.Definition)
+	}
+
+	return nil
 }
