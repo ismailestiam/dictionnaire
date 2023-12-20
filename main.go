8c80e03e85a0c230b71a82e8cb69962015f3bb37
ismailestiam <148719043+ismailestiam@users.noreply.github.com>
2023-12-21 00:54:17 +0100

    main.go

--- main.go
+++ main.go
@@ -1,44 +1,21 @@
+// main.go
 package main
 
 import (
 	"example/dictionnaire/dictionary"
-	"fmt"
+	"net/http"
 )
 
 func main() {
-	// Spécifiez le chemin du fichier pour le dictionnaire
 	filePath := "C:/Users/Lenovo/Desktop/workspace go/dictionnaire/entries.json"
 
-	// Créez une nouvelle instance de Dictionnaire avec le chemin du fichier
 	dict := dictionary.NewDictionnaire(filePath)
 
-	err := dict.List()
-	if err != nil {
-		fmt.Println("Erreur lors de l'affichage initial:", err)
-		return
-	}
+	// Configure HTTP routes
+	http.HandleFunc("/add", dict.AddEntryHandler)
+	http.HandleFunc("/remove", dict.RemoveEntryHandler)
+	http.HandleFunc("/list", dict.ListEntriesHandler)
 
-	// Obtenez la définition d'un mot
-	mot := "golang"
-	definition, err := dict.Get(mot)
-	if err != nil {
-		fmt.Println("Erreur lors de la récupération:", err)
-	} else {
-		fmt.Printf("La définition de '%s' est '%s'\n", mot, definition)
-	}
-
-	// Supprimez un mot
-	motASupprimer := "chat"
-	err = dict.Remove(motASupprimer)
-	if err != nil {
-		fmt.Println("Erreur lors de la suppression:", err)
-		return
-	}
-
-	// Affichez à nouveau la liste après la suppression
-	err = dict.List()
-	if err != nil {
-		fmt.Println("Erreur lors de l'affichage après suppression:", err)
-		return
-	}
+	// Start the HTTP server
+	http.ListenAndServe(":8080", nil)
 }
