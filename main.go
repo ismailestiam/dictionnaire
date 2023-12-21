8e4b1a15185fc77c36ff8a7d92341061a7beb20e
ismailestiam <148719043+ismailestiam@users.noreply.github.com>
2023-12-21 15:09:25 +0100

    main.go

--- main.go
+++ main.go
@@ -1,21 +1,23 @@
-// main.go
 package main
 
 import (
-	"example/dictionnaire/dictionary"
+	dict "example/dictionnaire/dictionary"
+	"fmt"
 	"net/http"
 )
 
 func main() {
-	filePath := "C:/Users/Lenovo/Desktop/workspace go/dictionnaire/entries.json"
-
-	dict := dictionary.NewDictionnaire(filePath)
-
-	// Configure HTTP routes
-	http.HandleFunc("/add", dict.AddEntryHandler)
-	http.HandleFunc("/remove", dict.RemoveEntryHandler)
-	http.HandleFunc("/list", dict.ListEntriesHandler)
-
-	// Start the HTTP server
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("-------- Calling rest api ---------------------------\n")
+	d := dict.Init()
+	http.HandleFunc("/get", d.GetHandler)       //http:localhost:8030/get?word=your_word
+	http.HandleFunc("/list", d.ListHandler)     //http:localhost:8030/list
+	http.HandleFunc("/remove", d.RemoveHandler) //http:localhost:8030/remove?key=your_key
+	http.HandleFunc("/add", d.AddHandler)       // curl -X POST -H "Content-Type: application/json" -d "{\"key\": \"VB.Net\", \"value\": \"VB.Net is a programming language\"}" http://localhost:8083/add
+	http.HandleFunc("/update", d.UpdateHanlder) // curl -X PUT -H "Content-Type: application/json" -d "{\"key\": \"existing_key\", \"newValue\": \"updated_value\"}" http://localhost:8080/update
+	port := 8083
+	fmt.Printf("Server is listening on port :%d...\n", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
